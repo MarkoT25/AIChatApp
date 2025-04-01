a321e2098d33d0090ef0cee1cb5428482899ef59
Marko Ticinovic <markoticinovic25@gmail.com>
2025-04-01 06:34:27 +0200

    Updated chatbox header user logo

--- chat-app/components/Chat/UserChatboxHeader.tsx
+++ chat-app/components/Chat/UserChatboxHeader.tsx
@@ -1,4 +1,5 @@
 import useUserStore from "@/store/useUserStore";
+import Image from "next/image";
 import React from "react";
 import { IoCamera, IoPerson } from "react-icons/io5";
 
@@ -7,7 +8,18 @@ const UserChatboxHeader = () => {
   return (
     <div className="w-full flex items-center justify-between border-b-[1.5px] border-gray-800 px-5 py-5">
       <div className="flex items-center justify-center gap-2">
-        <IoPerson className="size-[40px] rounded-full p-2 border-[1.5px] border-gray-500" />
+        {!selectedUser?.profilePic  && (
+          <IoPerson className="size-[40px] rounded-full p-2 border-[1.5px] border-gray-500" />
+        )}
+        {selectedUser?.profilePic && (
+          <Image
+            src={selectedUser?.profilePic}
+            alt="profile"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+        )}
         <p className="text-18 text-on-surface font-semibold">
           {selectedUser?.username}
         </p>
